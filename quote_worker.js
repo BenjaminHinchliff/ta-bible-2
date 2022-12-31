2fc3f9e28b32149ef4f853a7d3bd09375d44ffbd
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2022-12-31 01:27:03 -0800

    update exception handling behavior

--- quote_worker.js
+++ quote_worker.js
@@ -2,6 +2,7 @@ let index = null;
 
 var Module = {
   onRuntimeInitialized: function() {
+    console.log(Module);
     index = new Module.BibleIndex()
     postMessage(["index-complete", "Index loading completed. Waiting on input."])
   }
@@ -17,8 +18,9 @@ onmessage = ({ data: target }) => {
   try {
     const quote = Module.build_quote(index, target);
     postMessage(["quote", quote]);
-  } catch (e) {
-    const message = Module.get_exception_message(e);
-    postMessage(["status", `Failed to build quote: ${message}`]);
+  } catch (p) {
+    // https://github.com/emscripten-core/emscripten/blob/main/test/test_core.py#L1689
+    const message = Module.getExceptionMessage(p);
+    postMessage(["status", `Failed to build quote: ${message[1]}`]);
   }
 }
